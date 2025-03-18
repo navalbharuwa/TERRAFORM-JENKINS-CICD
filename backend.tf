8d0eba77a492e67ae243a43ce387355c4758113c
navalbharuwa <139853225+navalbharuwa@users.noreply.github.com>
2025-03-18 14:26:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "1hotstarbucket"
     key            = "my-terraform-environment/main"
     region         = "eu-north-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
